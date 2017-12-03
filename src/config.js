c803b99ff566b1185e776237bece660d421e931f
Unknown <sergey.mitchenko@gmx.de>
2017-12-03 21:45:16 +0100

    translation in config

--- src/config.js
+++ src/config.js
@@ -1,14 +1,14 @@
 const config = {
   author: "Sergey Mitchenko",
   title: "Naked Tree in a cold Winter",
-  instructions: "Die Generierung basiert auf dem Lindenmayer-System. Dabei wird ein Startwort(Axiom) und Regeln festgelegt. " +
-  "Die Regeln ersetzen dann einzelne Buchstaben vom aktuellen Wort mit einer in der Regel definierten Zeichenfolge. " +
-  "Der generierte String wird anschließend gezeichnet (einstellbare Parameter: Farbe, Breite, Länge). " +
-  "Die Anzahl der Generierungen kann im Menü unter dem Punkt 'Number' festgelegt werden (ich empfehle die Nummer nicht zu hoch zu setzen, da sonst es sehr lange dauern kann bis das LSystem gezeichnet wird). " +
-  "Unter dem Punkt 'Rule' kann man vordefinierte Regeln ausprobieren und sich generieren lassen. " +
-  "Man kann auch selber sich eine Regel erstellen/ausdenken, indem man 'Custom' im 'Rule' Menü auswählt, und dann im 'Rule Folder' sich Regeln definiert. " +
-  "Ganz oben im Menü kann man den Renderer auswählen (SVG oder WebGL), wobei ich den WebGl Renderer empfehle, da er schneller ist (SVG ist drin, damit man eine SVG-Grafik generieren kann). " +
-  "Zusätzlich kann man die Kamera kontrollieren, indem man ihre Höhe und Distanz einstellt. Zusätzlich gibt es die Option die Kamera um das Objekt rotieren zu lassen"
+  instructions: "The generation is based on the Lindenmayer-System. For the system a start string (Axiom) and rules are defined. " +
+  "The rules replace single letters of the current string with a string, which is defined in the production of the rule. " +
+  "The generated string then will be passed on to a interpreter and drawn/rendered (adjustable parameter: color, width, length). " +
+  "The number of generations can be set in the menu on the right side (I recommend you to not set the number to high, because the render process can be very long). " +
+  "In the dropdown menu 'Rule' you can choose and generate predefined rules. " +
+  "You can also create own rules by selecting 'Custom' and defining rules in the 'Rule Folder'. " +
+  "On the top of the menu you can select the renderer (SVG oder WebGL). I recommend you to use the WebGl Renderer, because he is much faster (SVGRenderer is included, because the Task was to generate a SVG-Graphic). " +
+  "Additionally you can control the camera by setting the distance and height and optionally activate the rotation option."
 };
 
 export default config;
